fb43cf38bc770a40b15be086e464b2efe04bd119
jazzzooo <38244149+jazzzooo@users.noreply.github.com>
2026-07-21 23:48:23 +0200

    chore: remove dead Zig files

--- src/jsc/static_export.zig
+++
@@ -1,9 +0,0 @@
-Type: type,
-symbol_name: []const u8,
-local_name: []const u8,
-
-Parent: type,
-
-pub fn wrappedName(comptime this: *const @This()) []const u8 {
-    return comptime "wrap" ++ this.symbol_name;
-}
